e420a92b1e515282fda91175950c2f8e60fe512e
Christian Stamati <christian.stamati@gmail.com>
2023-08-31 19:34:26 +0200

    Added project folder structure

---
+++ Assets/_Project/Scripts/Tests/Runtime/RuntimeExampleTests.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace CHS.UnityCI
+{
+    public class RuntimeExampleTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void RuntimeExampleTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator RuntimeExampleTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
