fd5234e5c5a0b9cf38a89c0a4627185c5bee9280
Christian Stamati <christian.stamati@gmail.com>
2023-09-08 19:18:53 +0200

    Deploy app

---
+++ Assets/_Project/Scripts/Runtime/CHS/UICounter.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CHS.UnityCI
+{
+    public class UICounter : MonoBehaviour
+    {
+        [SerializeField] private UIDocument document;
+        [SerializeField] private int count;
+        private Label _countLabel;
+        private Button _countButton;
+        
+        private void Awake()
+        {
+            var root = document.rootVisualElement;
+            _countLabel = root.Q<Label>("Count");
+            _countButton = root.Q<Button>("CountButton");
+        }
+
+        private void OnEnable()
+        {
+            _countButton.clicked += OnButtonPressed;
+        }
+
+        private void OnDisable()
+        {
+            _countButton.clicked -= OnButtonPressed;
+        }
+        
+        private void OnButtonPressed()
+        {
+            count++;
+            _countLabel.text = count.ToString();
+        }
+    }
+}
